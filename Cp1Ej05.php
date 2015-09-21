654d1cf5a310c8e502b7801cccf23ed8cd55b0a8
JuanAntonioRamirezParras <jaramirezparras@gmail.com>
2015-09-21 10:15:57 +0200

    Subiendo los ejercicios del capítulo 1

---
+++ Cp1Ej05.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $x = 144;
+        $y = 999;        
+        ?>
+        
+        Suma______________
+        <?php echo $x + $y, "<br>";?>
+        
+        Resta______________
+        <?php echo $x - $y, "<br>";?>
+        
+        Multiplicación_____
+        <?php echo $x * $y, "<br>";?>
+        
+        División___________
+        <?php echo $x / $y, "<br>";?>
+        
+        
+    </body>
+</html>
